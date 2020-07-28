edda59bf73d2feb6f1b53d7ea7ea759b16d1271e
jorge.hg <jorge.hg@globant.com>
2020-07-28 17:34:31 -0500

    Chore(*): set up PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
 const path = require('path')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
   output: {
@@ -21,6 +22,24 @@ module.exports = {
         src: path.resolve('src/assets/icon.png'),
         sizes: [96, 128, 192, 256, 384, 512]
       }]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://res.cloudinary.com|images.unsplash.com'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp('https://petgram-one-rho.vercel.app/'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
+        }
+      ]
     })
   ],
   module: {
